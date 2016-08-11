7c7cc02d850478b308afd8759e8c5097e3dab191
Hassan Karaouni <hkII@DN525ehn.SUNet>
2016-08-10 22:56:05 -0700

    Remove hell

--- Assets/Scripts/ItemInteractions/EarthInteraction.cs
+++ Assets/Scripts/ItemInteractions/EarthInteraction.cs
@@ -50,33 +50,6 @@ public class EarthInteraction : MonoBehaviour {
 		if (transform.up.y < yLower) {
 			transform.Rotate (new Vector3 (-rotateVector.x, 0, 0), Space.World);
 		}
-
-		/*
-		transform.Rotate (rotateVector, Space.World);
-
-		if (transform.forward.y > yUpper || transform.forward.y < yLower) {
-			transform.Rotate (-rotateVector, Space.World);
-		}
-		*/
-
-		if (Input.GetMouseButtonDown (0)) {
-			print ("Current Up: " + transform.up);
-		}
-
-
-		/*
-		xRotation += rotateVector.x;
-		xRotation = Mathf.Clamp (xRotation, -50f, 50f);
-		yRotation += rotateVector.y;
-
-		transform.eulerAngles = new Vector3 (transform.eulerAngles.x, yRotation, 0);
-		transform.eulerAngles = new Vector3 (xRotation, transform.eulerAngles.y, 0);
-
-		if (Input.GetMouseButtonDown (0)) {
-			print ("Global Euler: " + transform.eulerAngles);
-			print ("Local Euler: " + transform.localEulerAngles);
-		}
-		*/
 	}
 
 	private Vector3 GetRotationVector() {
