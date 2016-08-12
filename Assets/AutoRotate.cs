4d5a3775f8503dbee58c15cd97885836713ad168
Hassan Karaouni <hkII@Hassans-MBP-2.lan>
2016-08-12 15:59:32 -0700

    Sun prototype

---
+++ Assets/AutoRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoRotate : MonoBehaviour {
+	Vector3 rotation;
+
+	// Use this for initialization
+	void Start () {
+		rotation = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dt = Time.deltaTime;
+		rotation.x = rotation.y = rotation.z = dt;
+		transform.Rotate (rotation, Space.World);
+	}
+}
